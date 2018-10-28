dcfd59a3f9834d38b3a8a8d872f231ebcc382b14
Jeamar <jeamar1234@gmail.com>
2018-10-28 15:54:27 +0800

    udpates

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -4,8 +4,6 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-use DB;
-
 class CreateUsersTable extends Migration
 {
     /**
