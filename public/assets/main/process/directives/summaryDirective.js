7fd3e5d5faa545a937906054dc144e8f275a76f0
Jeamar <jeamar1234@gmail.com>
2019-06-13 09:57:40 +0800

    updates

--- public/assets/main/process/directives/summaryDirective.js
+++ public/assets/main/process/directives/summaryDirective.js
@@ -15,14 +15,22 @@ app.directive('summaryDirective', [
         console.log( "summaryDirective Runinng !" );
 
         scope.alphabet = [ 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' ];
-        scope.expensesChartLabels = ['A','B','C'];
-        scope.expensesChartData = [1,2,3];
+        scope.expensesPieChartLabels = ['A','B','C'];
+        scope.expensesPieChartData = [1,2,3];
+        scope.expensesLineChartLabels = ['A','B','C','D','E','F','G','H'];
+        scope.expensesLineChartData = [20,10,44,21,55,95,11,39];
+        scope.start_date = moment();
+        scope.end_date = moment();
+        scope.dateFilterVal = moment();
 
-        scope.initializeChart = ( ) =>{
-          $timeout(function() {
 
-          }, 300);
-          scope.expensesChartOptions = {
+
+
+        scope.toggleDatePicker = ( ) =>{
+          
+        }
+        scope.initializePieChart = ( ) =>{
+          scope.expensesPieChartOptions = {
             legend: {
               display: true,
               position: 'left',
@@ -34,12 +42,45 @@ app.directive('summaryDirective', [
             },
           }
         }
+        scope.initializeLineChart = ( ) =>{
+          scope.expensesLineChartOptions = {
+            
+          }
+        }
+        scope.initializeDatePickers = ( ) =>{
+          $('.datepicker-input').daterangepicker({
+            // timePicker: true,
+            // startDate: moment().startOf('hour'),
+            opens: 'left',
+            showDropdowns: true,
+            autoApply: true,
+            endDate: moment(),
+            locale: {
+              format: 'MMM DD, YYYY'
+            }
+          });
+          $('.datepicker-input').on('apply.daterangepicker', function(ev, picker) {
+            console.log( ev );
+            console.log( picker );
+          });
+        }
+        scope.initializeEventListeners = ( ) =>{
+          $("body").click(function(e){
+            if ( $(e.target).parents(".date-btn").length === 0) {
+            }
+          });
+        }
 
         scope.onLoad = ( ) =>{
-          scope.initializeChart();
+          scope.initializePieChart();
+          scope.initializeLineChart();
+          scope.initializeDatePickers();
+          scope.initializeEventListeners();
         }
 
         scope.onLoad();
+
+        
 
       }
     }
--- resources/views/main/index.blade.php
+++ resources/views/main/index.blade.php
@@ -9,9 +9,11 @@
     <title>Manage Expenses</title>
     <link rel="shortcut icon" href="{{ asset('img/logo/jl-logo-blue.png') }}" type="image/ico">
 
+    <link rel="stylesheet" type="text/css" href="../css/font-awesome.min.css">
     <link rel="stylesheet" type="text/css" href="../css/fonts.css">
     <link rel="stylesheet" type="text/css" href="../css/open-iconic.css">
     <link rel="stylesheet" type="text/css" href="../css/sweetalert.css">
+    <link rel="stylesheet" type="text/css" href="../css/daterangepicker.css">
     <link rel="stylesheet" type="text/css" href="../assets/main/css/style.css">
     <link rel="stylesheet" type="text/css" href="../assets/main/css/auth.css">
     <link rel="stylesheet" type="text/css" href="../assets/main/css/dashboard.css">
@@ -34,6 +36,7 @@
   <script type="text/javascript" src="<?php echo $server; ?>/js/angular-ui-router.min.js"></script>
   <script type="text/javascript" src="<?php echo $server; ?>/js/angular-local-storage.min.js"></script>
   <script type="text/javascript" src="<?php echo $server; ?>/js/sweetalert.min.js"></script>
+  <script type="text/javascript" src="<?php echo $server; ?>/js/daterangepicker.js"></script>
   
   
   <script type="text/javascript" src="<?php echo $server; ?>/assets/main/process/app.js"></script>
